013b8ed849bef875e092830c52fb3b185e4d18ef
Shahrad Elahi <shahrad@litehex.com>
2024-11-21 21:46:12 -1000

    fix

--- tooling/eslint/base.js
+++ tooling/eslint/base.js
@@ -6,5 +6,17 @@ export default tseslint.config(
     ignores: ['dist/**'],
   },
   eslint.configs.recommended,
-  tseslint.configs.recommended
+  tseslint.configs.recommended,
+  {
+    rules: {
+      'no-console': 'error',
+      '@typescript-eslint/semi': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-expressions': ['off', { allowShortCircuit: true }],
+      '@typescript-eslint/no-unused-vars': [
+        'off',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+    },
+  }
 );
